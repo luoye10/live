abfcc4b0237f7e8c12d15940bc4122973d0d34ad
zengyousen <511645424@qq.com>
2021-08-12 18:02:20 +0800

    添加结构说明

--- src/components/main.vue
+++ src/components/main.vue
@@ -1,3 +1,20 @@
 <template>
-  <div class="main"><el-skeleton :rows="6" animated /></div>
+	<div class="main">
+		<el-skeleton v-if="isLoading" :rows="6" animated />
+		<div class="layout">
+			<!-- 头部 -->
+			<!-- 左侧菜单 -->
+			<!-- 右侧展示 -->
+			<!-- 底部 -->
+		</div>
+	</div>
 </template>
+<script>
+export default {
+	data() {
+		return {
+			isLoading: true,
+		};
+	},
+};
+</script>
