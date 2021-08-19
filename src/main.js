e909a694f4f6cefa99ec3f1b52c122cae7ed5ec6
luoye <511645424@qq.com>
2021-08-19 22:11:20 +0800

    字体的引入

--- src/main.js
+++ src/main.js
@@ -3,10 +3,11 @@ import App from './App.vue';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import router from './router';
+import './css/global.css';
 
 Vue.config.productionTip = false;
 Vue.use(ElementUI);
 new Vue({
-    render: h => h(App),
-    router
+	render: h => h(App),
+	router,
 }).$mount('#app');
