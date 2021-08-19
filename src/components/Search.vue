ff7e7eac761e39b64c6060bf07f02f9ec042f22b
luoye <511645424@qq.com>
2021-08-19 22:16:33 +0800

    根据关键词搜索到的歌曲信息

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,98 +1,105 @@
 <template>
-    <div class="reveal">
-        <ul class="song-list">
-            <li class="song" v-for="item in items" :key="item.message" @click="button">
-                <img :src="item.album.artist.img1v1Url" class="img" />
-                <div class="content">
-                    <span class="name">{{ item.name }}</span>
-                    <span class="singer">{{ item.artists[0].name }}</span>
-                    <span class="album">{{ item.album.name }}</span>
-                </div>
-            </li>
-        </ul>
-    </div>
+	<div class="reveal">
+		<ul class="song-list">
+			<li class="song" v-for="item in items" :key="item.message" @click="button(item)">
+				<img :src="item.img" class="img" />
+				<span class="name item">{{ item.name }}</span>
+				<span class="singer item">{{ item.singer }}</span>
+				<span class="album item">{{ item.album }}</span>
+				<span class="time item">{{ item.dt }}</span>
+			</li>
+		</ul>
+	</div>
 </template>
 <script>
-import api from '../api/index'
+import api from '../api/index';
+import format from '../tool';
 export default {
-    props: [
-        'word'
-    ],
-    data(){
-        return {
-            items: []
-        }
-    },
-    mounted(){
-        this.searchRequest()
-    },
-    methods: {
-        searchRequest(){
-            if (!this.word) {
+	props: ['word'],
+	data() {
+		return {
+			items: [],
+		};
+	},
+	mounted() {
+		this.searchRequest();
+	},
+	methods: {
+		searchRequest() {
+			if (!this.word) {
 				this.$message.warning('请输入要搜索的关键词');
 				return;
 			}
 			api.query(this.word)
 				.then(response => {
-					console.log(response);
-					this.items = response.data.result.songs;
+					const list = response.data.result.songs;
+					const items = list.map(item => {
+						item.img = item.artists[0].img1v1Url;
+						item.dt = format(item.duration);
+						item.singer = item.artists[0].name;
+						item.album = item.album.name;
+						return item;
+					});
+					this.items = items;
 				})
 				.catch(function (error) {
 					console.log(error);
 				});
-        },
-        button(){
-
 		},
-    }
-}
+	},
+};
 </script>
 <style lang="less" scoped>
-img,div,span,ul,li{
-    margin: 0;
-    padding: 0;
+img,
+div,
+span,
+ul,
+li {
+	margin: 0;
+	padding: 0;
 }
 .reveal {
-    position: absolute;
-    left: 400px;
-    top: 100px;
-    right: 0;
-    .song {
-        list-style: none;
-        height: 100px;
-        border: 1px solid aqua;
-        border-radius: 5px;
-        background: rgba(129, 89, 89, 0.493);
-        vertical-align: middle;
-        line-height: 100px;
-        cursor: pointer;
-        margin: 20px;
-        .img {
-            width: 50px;
-            height: 50px;
-            border-radius: 50%;
-            vertical-align: middle;
-            line-height: 100px;
-        }
-        .content {
-            display: inline-block;
-            vertical-align: middle;
-            line-height: 100px;
-            span{
-                display: inline-block;
-                color: aqua;
-                margin: 0 10px;
-            }
-            .name{
-                width: 150px;
-            }
-            .singer{
-                width: 100px;
-            }
-            .album{
-                width: 150px;
-            }
-        }
-    }
+	position: absolute;
+	left: 30%;
+	top: 100px;
+	right: 0;
+	.song {
+		list-style: none;
+		height: 100px;
+		border: 1px solid aqua;
+		border-radius: 5px;
+		background: rgba(129, 89, 89, 0.493);
+		vertical-align: middle;
+		line-height: 100px;
+		cursor: pointer;
+		margin: 20px;
+		.img {
+			width: 50px;
+			height: 50px;
+			border-radius: 50%;
+			vertical-align: middle;
+			line-height: 100px;
+			margin: 0 20px;
+		}
+		.item {
+			display: inline-block;
+			vertical-align: middle;
+			text-align: center;
+			color: aqua;
+			font-size: 20px;
+		}
+		.name {
+			width: 30%;
+		}
+		.singer {
+			width: 15%;
+		}
+		.album {
+			width: 30%;
+		}
+		.time {
+			width: 10%;
+		}
+	}
 }
 </style>
