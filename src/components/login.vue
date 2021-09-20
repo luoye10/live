0455a6723d54b40323e896f8b59b9fb0ed6402da
落叶 <63185709+luoye10@users.noreply.github.com>
2021-09-20 15:00:26 +0800

    绑定keyup事件

--- src/components/login.vue
+++ src/components/login.vue
@@ -6,7 +6,7 @@
 					<el-input v-model="ruleForm.username"></el-input>
 				</el-form-item>
 				<el-form-item placeholder="密码" prop="password">
-					<el-input v-model="ruleForm.password" type="password"></el-input>
+					<el-input v-model="ruleForm.password" type="password" @keyup.enter.native="login"></el-input>
 				</el-form-item>
 			</el-form>
 			<el-button type="primary" @click="login">登录</el-button>
@@ -38,26 +38,7 @@ export default {
 			},
 		};
 	},
-	mounted() {
-		window.addEventListener('keyup', this.keyogin);
-	},
-	beforeDestroy() {
-		window.removeEventListener('keyup', this.keyogin);
-	},
 	methods: {
-		keyogin(e) {
-			if (e.code === 'Enter') {
-				// if (this.ruleForm.username && this.ruleForm.password) {
-				// 	this.login();
-				// }
-				this.$refs.loginForm.validate(valid => {
-					if (!valid) {
-						return;
-					}
-					this.login();
-				});
-			}
-		},
 		login() {
 			this.$refs['loginForm'].validate(valid => {
 				if (!valid) {
